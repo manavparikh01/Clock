f9761fa6f9dd071c0cc53e12fe2bec2186dcac93
manavparikh01 <manavparikh01@gmail.com>
2021-07-03 02:16:59 +0530

    bug small one

--- assets/index.js
+++ assets/index.js
@@ -6,6 +6,12 @@ let date = document.querySelector('.date')
 let twofour = document.querySelector('.twofour')
 let onetwo = document.querySelector('.onetwo')
 let timeagain = document.querySelector('.timeagain')
+let clock = document.querySelector('.cl')
+let timer = document.querySelector('.tm')
+let stopwatch = document.querySelector('.st')
+let clocka = document.querySelector('.cl a')
+let timera = document.querySelector('.tm a')
+let stopwatcha = document.querySelector('.st a')
 let stamp = 0
 
 twofour.addEventListener('click', () => {
@@ -22,6 +28,47 @@ onetwo.addEventListener('click', () => {
     twofour.classList.remove('selectedtimeformat')
     twofour.classList.add('unselectedformat')
     stamp = 12
+})
+
+clock.addEventListener('click', () => {
+    timer.classList.add('unselectedformat')
+    timer.classList.remove('selectedtimeformat')
+    stopwatch.classList.add('unselectedformat')
+    stopwatch.classList.remove('selectedtimeformat')
+    clock.classList.remove('unselectedformat')
+    clock.classList.add('selectedtimeformat')
+    timera.classList.add('unself')
+    timera.classList.remove('seltf')
+    stopwatcha.classList.add('unself')
+    stopwatcha.classList.remove('seltf')
+    clocka.classList.remove('unself')
+    clocka.classList.add('seltf')
+    stamp = 24
+})
+
+timer.addEventListener('click', () => {
+    timer.classList.remove('unselectedformat')
+    timer.classList.add('selectedtimeformat')
+    stopwatch.classList.add('unselectedformat')
+    stopwatch.classList.remove('selectedtimeformat')
+    clock.classList.add('unselectedformat')
+    clock.classList.remove('selectedtimeformat')
+})
+
+stopwatch.addEventListener('click', () => {
+    timer.classList.add('unselectedformat')
+    timer.classList.remove('selectedtimeformat')
+    stopwatch.classList.remove('unselectedformat')
+    stopwatch.classList.add('selectedtimeformat')
+    clock.classList.add('unselectedformat')
+    clock.classList.remove('selectedtimeformat')
+    timera.classList.add('unself')
+    timera.classList.remove('seltf')
+    stopwatcha.classList.remove('unself')
+    stopwatcha.classList.add('seltf')
+    clocka.classList.add('unself')
+    clocka.classList.remove('seltf')
+    stamp = 24
 })
 
 
@@ -51,6 +98,8 @@ let onLoad = () => {
     //     window.onload()
     //   }, 1000);
 }
+
+onLoad()
 
 setInterval(function() {
     onLoad();
